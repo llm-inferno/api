db98d45043fb3473eef441f1b8f92ca3a510cd34
tantawi <tantawi@us.ibm.com>
2025-06-02 16:38:33 -0400

    remove v1beta1

--- api/v1beta1/defaults.go
+++
@@ -1,7 +0,0 @@
-package v1beta1
-
-// default name of a service class
-const DefaultServiceClassName string = "Free"
-
-// default priority of a service class
-const DefaultServiceClassPriority int = 0
